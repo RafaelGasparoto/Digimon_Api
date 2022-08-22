5c8dd8ab02738e4f98943eee34b58f7ed9f9592b
Rafael <rafaelgasparoto4@gmail.com>
2022-08-21 21:02:24 -0300

    Estado do app que usei somente para puxar todos os nomes dos digimons e armazenar em um arquivo para utilizar na sugestão de pesquisa

--- lib/API.dart
+++ lib/API.dart
@@ -1,6 +1,6 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
-
+import 'dart:io';
 import 'model/Digimon.dart';
 import 'package:http/http.dart' as http;
 
@@ -16,7 +16,11 @@ class API{
     List<Digimon> digimon = dadosJson['content'].map<Digimon>((map) {
       return Digimon.fromJson(map);
     }).toList();
-    print('digi> ${digimon[1421].name}');
+    for(var i = 0; i <1422; i++){
+      print("n: ${digimon[i].name}");
+    }
+
+    // Close the IOSink to free system resources.
     /*Map<String, dynamic> dadosJson = json.decode(response.body);
     Digimon digimon = Digimon.fromJson(dadosJson);*/
     return digimon;
